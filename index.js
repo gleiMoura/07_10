1049debfae8ca3c2d19380b8309766a132f4fddf
gleiMoura <gle.bar.mou@gmail.com>
2024-10-09 20:20:10 -0300

    ✨ feat: adding itteration in project

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const caixas = document.querySelector(".caixas");
+
 const createCards = () => {
-    let data = [];
-    axios.get("LINK").then((response) => {
-        data = response.data;
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+        const info = response?.data;
+
+        for (const card of info) {
+            caixas.innerHTML += `
+                <div class="caixa">
+                    <h2>${card.title}</h2>
+                    <p>${card.body}</p>
+                </div>
+            `
+        }
     }).catch((error) => {
-        console.log("deu um erro cabuloso, o que será que foi?",error);
+        console.log("deu um erro cabuloso, o que será que foi?", error);
         console.error(error);
     })
 };
